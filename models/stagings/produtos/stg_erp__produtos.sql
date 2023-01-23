997ace38400bdd190e173bee145ab1f96d0f92f5
felipe7373@hotmail.com <felipe7373@hotmail.com>
2023-01-23 21:45:58 +0000

    ajuste em staging

--- models/stagings/produtos/stg_erp__produtos.sql
+++ models/stagings/produtos/stg_erp__produtos.sql
@@ -14,6 +14,19 @@ with
             , cast(listprice as float64) as lista_preco
             , cast(`size` as string) as tamanho
             , cast(sizeunitmeasurecode as string) as unid_medida
+            , cast(weightunitmeasurecode as string) as unid_peso
+            , cast(weight as float64) as peso
+            , cast(daystomanufacture as int) as dias_producao
+            , cast(productline as string) as linha_produto
+            , cast(class as string) as classe
+            , cast(style as string) as estilo
+            , cast(productsubcategoryid as int) as id_subcat
+            , cast(productmodelid as int) as id_modelo
+            , cast(left(sellstartdate,10) as date) as data_venda_ini
+            , cast(left(sellenddate,10) as date) as data_venda_fin
+            , cast(discontinueddate as int) as descontinuado
+            , cast(rowguid as string) as rowguid
+            , cast(left(modifieddate,10) as date) as data_mod
 
         from {{ source('erp', 'product') }}
     )
--- models/stagings/vendas/stg_erp__vendas-header.sql
+++ models/stagings/vendas/stg_erp__vendas-header.sql
@@ -8,7 +8,6 @@ with
             , cast(left(duedate,10) as date) as data_prevista			
             , cast(left(shipdate,10) as date) as data_entrega			
             , cast(status as int) as status	
-            --, cast(onlineorderflag as string) as venda_online	
             , case
                 when onlineorderflag = FALSE then "nao"
                 when onlineorderflag = TRUE then "sim"
@@ -19,6 +18,19 @@ with
             , cast(customerid as int) as id_cliente			
             , cast(salespersonid as int) as id_vendedor			
             , cast(territoryid as int) as id_regiao
+            , cast(billtoaddressid as int) as id_endereco_cobranca
+            , cast(shiptoaddressid as int) as id_endereco_entrega
+            , cast(shipmethodid as int) as id_tipo_envio
+            , cast(creditcardid as int) as id_cartao
+            , cast(creditcardapprovalcode as string) as cod_aprovacao
+            , cast(currencyrateid as int) as id_cambio
+            , cast(subtotal as float64) as subtotal
+            , cast(taxamt as float64) as taxa_amt
+            , cast(freight as float64) as frete
+            , cast(totaldue as float64) as total_atual
+            , cast(comment as int) as comment
+            , cast(rowguid as string) as rowguid
+            , cast(left(modifieddate,10) as date) as data_mod
 
         from {{ source('erp', 'salesorderheader') }}
     )
